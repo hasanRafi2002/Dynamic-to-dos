3981488f540a9511419932d762d040dccaddea3e
hasanRafi2002 <rafi2002ten@gmail.com>
2025-04-06 21:39:35 +0600

    rafi

---
+++ to-dos-m/demo.swift
@@ -0,0 +1,7 @@
+//
+//  demo.swift
+//  to-dos-m
+//
+//  Created by Md Tawhid Hasan Rafi on 6/4/25.
+//
+
---
+++ to-dos-m/to_dos_mApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_dos_mApp.swift
+//  to-dos-m
+//
+//  Created by Md Tawhid Hasan Rafi on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct to_dos_mApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
